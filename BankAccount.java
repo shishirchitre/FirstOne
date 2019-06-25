bdcaa7eaecd1853a13df15bea0bb71fc5967ffc7
Shishir Chitre <32685997+shishirchitre@users.noreply.github.com>
2019-06-25 22:10:21 +0530

    Add files via upload
    First Java File.

---
+++ BankAccount.java
@@ -0,0 +1,80 @@
+
+
+
+package com.shishirchitre;
+
+public class BankAccount {
+
+    String customerAccountNumber;
+    double customerBalance;
+    String customerName;
+    String customerEmailID;
+    String customerPhoneNo;
+
+    public static void main(String [] args){
+
+    }
+
+    public void withdrawal(double withdrawalAmount){
+        if(this.customerBalance - withdrawalAmount < 0){
+            System.out.println("Insufficient Funds! Sorry.");
+        }
+        else {
+            this.customerBalance -= withdrawalAmount;
+            System.out.println("Withdrawal of  " + withdrawalAmount + " is do ne successfully! Now the current balance is  " + customerBalance);
+        }
+    }
+    public void deposit(double depositAmount){
+        this.customerBalance += depositAmount;
+        System.out.println("Deposit of  " + depositAmount + " is do ne successfully! Now the current balance is  " + customerBalance);
+    }
+
+
+
+
+    public String getCustomerAccountNumber() {
+        return customerAccountNumber;
+    }
+
+    public void setCustomerAccountNumber(String customerAccountNumber) {
+        this.customerAccountNumber = customerAccountNumber;
+    }
+
+    public double getCustomerBalance() {
+        return customerBalance;
+    }
+
+    public void setCustomerBalance(double customerBalance) {
+        this.customerBalance = customerBalance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmailID() {
+        return customerEmailID;
+    }
+
+    public void setCustomerEmailID(String customerEmailID) {
+        this.customerEmailID = customerEmailID;
+    }
+
+    public String getCustomerPhoneNo() {
+        return customerPhoneNo;
+    }
+
+    public void setCustomerPhoneNo(String customerPhoneNo) {
+        this.customerPhoneNo = customerPhoneNo;
+    }
+
+
+
+
+
+
+}
